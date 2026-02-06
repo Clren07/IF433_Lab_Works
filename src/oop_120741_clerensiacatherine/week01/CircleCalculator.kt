8ac00de6b7d3fd3437d1021cb135d1eca0e69da6
Clren07 <clerensiacatherine@gmail.com>
2026-02-06 16:11:12 +0700

    week01: (tugas 1) refactor variables to val and use string templates

--- src/oop_120741_clerensiacatherine/week01/CircleCalculator.kt
+++ src/oop_120741_clerensiacatherine/week01/CircleCalculator.kt
@@ -2,14 +2,14 @@ package oop_120741_clerensiacatherine.week01
 
 fun main (args: Array<String>) {
     //Variable definition
-    var radius: Double = 7.0
-    var pi: Double = 13.4
+    val radius = 7.0
+    val pi = 13.4
 
     //Calculation
     var area: Double = radius * radius * pi
 
     //Output Concatenation
-    println("Radius: " + radius + ", Area: " + area)
+    println("Radius: $radius , Area: $area")
 
     //Logic Check
     checkSize(area)
