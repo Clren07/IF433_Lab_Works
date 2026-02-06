1a84313e5ac3ef8210cf335b48cbf04fc5232e2a
Clren07 <clerensiacatherine@gmail.com>
2026-02-06 15:42:29 +0700

    week01: initial setup and procedural code

---
+++ src/oop_120741_clerensiacatherine/week01/GradingSystem.kt
@@ -0,0 +1,10 @@
+package oop_120741_clerensiacatherine.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 0
+
+    //Concatenation gaya lama
+    println("Name: " + name + ", Score: " + score)
+
+}
