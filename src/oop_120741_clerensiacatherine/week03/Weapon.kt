a465279144dc47d45daa9bd3567b87b792b08963
Clren07 <clerensiacatherine@gmail.com>
2026-02-20 15:25:58 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_120741_clerensiacatherine/week03/Weapon.kt
+++ src/oop_120741_clerensiacatherine/week03/Weapon.kt
@@ -1,14 +1,14 @@
 package oop_120741_clerensiacatherine.week03
 
-class Weapon  (val name: String){ //Val (read-only) tidak bisa diubah
+class Weapon  (val name: String) { //Val (read-only) tidak bisa diubah
     var damage: Int = 0
         set(value) {
-            if (value < 0){
+            if (value < 0) {
                 println("Peringatan damage tidak boleh bernilai negatif!")
                 //nilai field tidak berubah
-            } else if (value > 1000){
+            } else if (value > 1000) {
                 println("Peringatan Overpowered. Damage diatur menjadi 1000")
-                field=1000
+                field = 1000
             } else {
                 field = value //nilai field berubah sesuai setter
             }
@@ -20,4 +20,18 @@ class Weapon  (val name: String){ //Val (read-only) tidak bisa diubah
             damage > 500 -> "Epic"
             else -> "Common"
         }
+
+    fun printStatus() {
+        println("Senjata $name, Damage: $damage, Tier: $tier")
+    }
+}
+
+fun main(){
+    val w = Weapon("AK-94")
+
+    //Test Validasi Damage
+    w.damage = -50 //Error
+    w.damage = 9999 //Set ke 1000
+
+    w.printStatus()
 }
