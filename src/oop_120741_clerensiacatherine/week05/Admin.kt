78931dc56c60e14fd18ebabfa360019dcebd8459
Clren07 <clerensiacatherine@gmail.com>
2026-03-06 13:12:22 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/oop_120741_clerensiacatherine/week05/Admin.kt
@@ -0,0 +1,10 @@
+package oop_120741_clerensiacatherine.week05
+
+class Admin (nama: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang duduk di depan kommputer melayani administrasi.)
+    }
+    fun doAdminWork(){
+        println("[$nama] sedang mmerekap data absensi mahasiswa.")
+    }
+}
