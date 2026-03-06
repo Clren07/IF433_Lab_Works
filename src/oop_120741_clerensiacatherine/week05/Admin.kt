f122bf662e4ac2a356f2cfe18cc761e3522ecef8
Clren07 <clerensiacatherine@gmail.com>
2026-03-06 13:36:31 +0700

    week05: implement polymorphic collection and smart casting

--- src/oop_120741_clerensiacatherine/week05/Admin.kt
+++ src/oop_120741_clerensiacatherine/week05/Admin.kt
@@ -2,7 +2,7 @@ package oop_120741_clerensiacatherine.week05
 
 class Admin (nama: String) : Pegawai(nama) {
     override fun bekerja() {
-        println("[$nama] sedang duduk di depan kommputer melayani administrasi.)
+        println("[$nama] sedang duduk di depan kommputer melayani administrasi.")
     }
     fun doAdminWork(){
         println("[$nama] sedang mmerekap data absensi mahasiswa.")
