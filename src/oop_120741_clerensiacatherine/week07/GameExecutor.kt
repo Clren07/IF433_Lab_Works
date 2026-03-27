ce58efd23258fef4699c302218ceef0d0f4b6811
Clren07 <clerensiacatherine@gmail.com>
2026-03-27 15:53:12 +0700

    week07: (task) implement exhaustive processEvent

---
+++ src/oop_120741_clerensiacatherine/week07/GameExecutor.kt
@@ -0,0 +1,23 @@
+package oop_120741_clerensiacatherine.week07
+
+class GameExecutor (){
+    fun processEvent(event: BattleState) {
+        when (event) {
+            is BattleState.MonsterEncounter -> {
+                println("Monster muncul: ${event.monsterName}")
+            }
+
+            is BattleState.LootDropped -> {
+                println("Dapat item: ${event.item.name} (Rarity: ${event.item.rarity})")
+            }
+
+            is BattleState.GameOver -> {
+                println("Game Over: ${event.reason}")
+            }
+
+            BattleState.SafeZone -> {
+                println("Kamu berada di zona aman.")
+            }
+        }
+    }
+}
