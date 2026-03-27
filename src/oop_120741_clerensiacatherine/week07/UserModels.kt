722d8b2429866b0cb3b0a9e0b760c858b32a2a6e
Clren07 <clerensiacatherine@gmail.com>
2026-03-27 15:08:32 +0700

    week07: implement data class for structural equality

--- src/oop_120741_clerensiacatherine/week07/UserModels.kt
+++ src/oop_120741_clerensiacatherine/week07/UserModels.kt
@@ -1,5 +1,4 @@
 package oop_120741_clerensiacatherine.week07
 
-class RegularUser(val name: String, val age: Int) {
-
-}
+class RegularUser(val name: String, val age: Int)
+data class DataUser (val name: String, val age: Int)
