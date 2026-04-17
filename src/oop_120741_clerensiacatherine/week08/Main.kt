975815397429b5eaa92b8d48873ec04411624aba
Clren07 <clerensiacatherine@gmail.com>
2026-04-17 21:45:49 +0700

    week08: implement chained safe calls and elvis

---
+++ src/oop_120741_clerensiacatherine/week08/Main.kt
@@ -0,0 +1,13 @@
+package oop_120741_clerensiacatherine.week08
+
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    val destination = emptyOrder.deliveryDetails
+        ?.address
+        ?.city
+        ?.name ?: "Kota Tidak Diketahui"
+
+    println("Tujuan pengiriman: $destination")
+}
