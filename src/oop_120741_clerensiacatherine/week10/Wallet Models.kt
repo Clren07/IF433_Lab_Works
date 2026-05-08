ded3cb3cd41d137be9da0821151b2dc4a7c04a39
Clren07 <clerensiacatherine@gmail.com>
2026-05-08 09:10:41 +0700

    week10: (task) implement search logic in repository

--- src/oop_120741_clerensiacatherine/week10/Wallet Models.kt
+++ src/oop_120741_clerensiacatherine/week10/Wallet Models.kt
@@ -1,9 +1,13 @@
 package oop_120741_clerensiacatherine.week10
 
+interface NamedEntity {
+    val name: String
+}
+
 data class Coin(
-    val name: String,
+    override val name: String,
     val balance: Double
-)
+) : NamedEntity
 
 data class Transaction(
     val id: String,
--- src/oop_120741_clerensiacatherine/week10/WalletRepository.kt
+++ src/oop_120741_clerensiacatherine/week10/WalletRepository.kt
@@ -1,12 +1,20 @@
 package oop_120741_clerensiacatherine.week10
 
-class WalletRepository<T> {
+
+class WalletRepository<T : Any> {
     private val items = mutableListOf<T>()
+
     fun add(item: T) {
         items.add(item)
     }
 
     fun getAll(): List<T> {
         return items
+    }
+
+    fun searchByName(name: String): List<T> {
+        return items.filter {
+            it is NamedEntity && it.name.contains(name, ignoreCase = true)
+        }
     }
 }
