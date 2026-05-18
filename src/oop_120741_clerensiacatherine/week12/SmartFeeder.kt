82a0929bce831423371b278c7628cb1274ca65ef
Clren07 <clerensiacatherine@gmail.com>
2026-05-18 14:42:17 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_120741_clerensiacatherine/week12/SmartFeeder.kt
+++ src/oop_120741_clerensiacatherine/week12/SmartFeeder.kt
@@ -5,7 +5,6 @@ fun dispenseKibble(
     availableGram: Int,
     isJammed: Boolean
 ): Int {
-
     require(requestedGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
     }
@@ -17,65 +16,44 @@ fun dispenseKibble(
     if (requestedGram > availableGram) {
         throw FoodEmptyException(requestedGram, availableGram)
     }
-
     println("Kibble berhasil dikeluarkan!")
-
     return availableGram - requestedGram
 }
 
 fun main() {
-
     var currentKibbleStock = 50
-
     // Jadwal makan pagi
     try {
-
         currentKibbleStock = dispenseKibble(
             requestedGram = 80,
             availableGram = currentKibbleStock,
             isJammed = false
         )
-
     } catch (e: DispenserJamException) {
-
         println(e.message)
-
     } catch (e: FoodEmptyException) {
-
         println(e.message)
-
     } catch (e: Exception) {
-
         println("Terjadi error umum: ${e.message}")
-
     } finally {
-
         println("Siklus pengecekan dispenser pagi selesai.")
     }
-
     // Jadwal makan sore
     runCatching {
-
         dispenseKibble(
             requestedGram = 30,
             availableGram = 1000,
             isJammed = false
         )
-
     }.onSuccess { newStock ->
-
         currentKibbleStock = newStock
-
         println(
             "Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr"
         )
-
     }.onFailure { error ->
-
         println(
             "Peringatan ke Pemilik: ${error.message}"
         )
-
         println(
             "(Opsional: Berikan chicken jerky secara manual)"
         )
