b0bdbb681de3adc6d930b04df91f4c65cbfd30c9
Clren07 <clerensiacatherine@gmail.com>
2026-05-22 15:49:34 +0700

    week13: test complete student serialization pipeline

--- src/oop_120741_clerensiacatherine/week13/StudentSerialization.kt
+++ src/oop_120741_clerensiacatherine/week13/StudentSerialization.kt
@@ -1,20 +1,27 @@
 package oop_120741_clerensiacatherine.week13
+
 import java.io.File
 import java.io.FileNotFoundException
 
 data class Student(val name: String, val age: Int, val gpa: Double)
 
 // Serialization (Object -> CSV)
-fun Student.toCSV(): String = "Name, Sage, Sqa"
+fun Student.toCSV(): String = "$name,$age,$gpa"
 
 // Deserialization (CSV -> Object)
 fun fromCSV(line: String): Student {
     val parts = line.split(",")
-    return Student(parts[0],parts[1].toInt(), parts[2].toDouble())
+    return Student(
+        parts[0],
+        parts[1].toInt(),
+        parts[2].toDouble()
+    )
 }
 
 fun saveStudent(student: List<Student>, path: String) {
-    File(path).writeText(student.joinToString ("\n") {it.toCSV()})
+    File(path).writeText(
+        student.joinToString("\n") { it.toCSV() }
+    )
 }
 
 fun loadStudent(path: String): List<Student> {
@@ -29,12 +36,13 @@ fun loadStudent(path: String): List<Student> {
 fun main() {
     val students = listOf(
         Student("Alice", 20, 3.8),
-        Student("Bob", 22,  3.5)
+        Student("Bob", 22, 3.5)
     )
 
     saveStudent(students, "students.csv")
 
     val loaded = loadStudent("students.csv")
+
     println("=== LOADED STUDENT DATA ====")
     loaded.forEach { println(it) }
 }
