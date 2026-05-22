8e7efde391505d4c316f2a0aaecb4596c16f8c7e
Clren07 <clerensiacatherine@gmail.com>
2026-05-22 14:54:15 +0700

    week13: implement readText and readLines

--- src/oop_120741_clerensiacatherine/week13/BasicFileIO.kt
+++ src/oop_120741_clerensiacatherine/week13/BasicFileIO.kt
@@ -1,12 +1,22 @@
 package oop_120741_clerensiacatherine.week13
 import java.io.File
 
-fun main(){
+fun main() {
     println("=== TEST WRITE TEXT ===")
-    val file = File ("notes.txt")
+    val file = File("notes.txt")
     file.writeText("Line 1: Inisialisasi sistem.\n")
     println("File berhasil dibuat dan ditulis")
 
     file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
     println("Text berhasil di-append")
+
+    println("\n=== TEST READ TEXT ===")
+    val fullContent = file.readText() // Mengambil seluruh isi file sebagai satu String
+    println("Membaca sekaligus:\n$fullContent")
+
+    println("=== TEST READ LINES ===")
+    val lines = file.readLines() // Mengambil per baris sebagai List<String>
+    lines.forEachIndexed { index, line ->
+        println("Isi Index $index: $line")
+    }
 }
